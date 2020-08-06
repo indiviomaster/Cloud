00383d886f92733524de3c8e3654897ab76b9733
indivioM <indivio@yandex.ru>
2020-08-07 00:27:01 +0300

    сериализация

--- client/src/main/java/Controller.java
+++ client/src/main/java/Controller.java
@@ -35,7 +35,7 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO: 7/21/2020 init connect to server
+
         //try{
             InetSocketAddress serverAddress = new InetSocketAddress("localhost", 8189);
             try {
@@ -64,119 +64,13 @@ public class Controller implements Initializable {
                 System.out.println("send file complete");
 
 
-  /*
-            Thread.sleep(1000);
-            clientFileList = new ArrayList<>();
-            String clientPath = "./client/src/main/resources/";
-            File dir = new File(clientPath);
-            if (!dir.exists()) {
-                throw new RuntimeException("directory resource not exists on client");
-            }
-            for (File file : Objects.requireNonNull(dir.listFiles())) {
-                clientFileList.add(file);
-                listView.getItems().add(file.getName());
-            }
 
-            Thread clientThread = new Thread(() -> {
-
-                try {
-                    while (true) {
-                        String command = is.readUTF();
-                        if (command.equals("./download")) {
-                            String fileNameD = is.readUTF();
-                            System.out.println(fileNameD);
-                            System.out.println("fileName: " + fileNameD);
-                            long fileLength = is.readLong();
-                            System.out.println("fileLength: " + fileLength);
-                            File fileD = new File(clientPath + fileNameD);
-                            if (!fileD.exists()) {
-                                fileD.createNewFile();
-                            }
-                            byte [] buffer = new byte[1024];
-                            try (FileOutputStream fos = new FileOutputStream(fileD)) {
-                                for (long i = 0; i < (fileLength / 1024 == 0 ? 1 : fileLength / 1024); i++) {
-                                    int bytesRead = is.read(buffer);
-                                    fos.write(buffer, 0, bytesRead);
-                                }
-                            }
-
-                            os.writeUTF("файл получен клиентом OK");
-                            clientFileList.clear();
-                            listView.getItems().clear();
-                            File dir2 = new File(clientPath);
-                            if (!dir2.exists()) {
-                                throw new RuntimeException("directory resource not exists on client");
-                            }
-                            for (File fileInDir : Objects.requireNonNull(dir2.listFiles())) {
-                                clientFileList.add(fileInDir);
-                                listView.getItems().add(fileInDir.getName());
-                            }
-                        } else if (command.equals(".err")){
-                            String msgErr = is.readUTF();
-                            System.out.println(msgErr);
-                        }
-                    }
-                }
-
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            clientThread.start();
-
-            listView.setOnMouseClicked(a -> {
-                if (a.getClickCount() == 2) {
-                    String fileName = listView.getSelectionModel().getSelectedItem();
-                    File currentFile = findFileByName(fileName);
-                    if (currentFile != null) {
-                        try {
-                            os.writeUTF("./upload");
-                            os.writeUTF(fileName);
-                            os.writeLong(currentFile.length());
-                            FileInputStream fis = new FileInputStream(currentFile);
-                            byte [] buffer = new byte[1024];
-                            while (fis.available() > 0) {
-                                int bytesRead = fis.read(buffer);
-                                os.write(buffer, 0, bytesRead);
-                            }
-                            os.flush();
-                            String response = is.readUTF();
-                            System.out.println(response);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
-
-
-*/
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
- /*   private File findFileByName(String fileName) {
-        for (File file : clientFileList) {
-            if (file.getName().equals(fileName)){
-                return file;
-            }
-        }
-        return null;
-    }*/
-    public void refreshListView(){
-        Platform.runLater(()->{
-            try {
-                listView.getItems().clear(); // очистка listView
-                Files.list(Paths.get(clientPath))
-                        .filter(path -> !Files.isDirectory(path))
-                        .map(path->path.getFileName().toString())
-                        .forEach(fname->listView.getItems().add(fname));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
-        });
-    }
+
 
 }
